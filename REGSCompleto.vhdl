bfd5ff11ffbe0af3352e34dc5c721842551150e3
Henrique <hss@outlook.com.br>
2020-10-11 23:16:09 -0300

    Ajustes

--- REGSCompleto.vhdl
+++ REGSCompleto.vhdl
@@ -1,19 +1,17 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
--- pagina 465 pdf
 ENTITY REGS IS
-	PORT( RegWrite, load, reset		:IN	STD_LOGIC; 							-- sinal do controlador e sinais de load e reset
-			regA, regB, writeR			:IN	STD_LOGIC_VECTOR(1 DOWNTO 0); -- Controle dos registradores a serem usados
-			data								:IN	STD_LOGIC_VECTOR(7 DOWNTO 0); -- Informações a serem escritas em um registrador
-			R0, R1, R2, R3					:BUFFER STD_LOGIC_VECTOR(7 DOWNTO 0); -- Registradores
-			Rdata1, Rdata2					:OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); -- saída de informações dos registradores
+	PORT( 	RegWrite, load, reset			:IN	STD_LOGIC;							-- sinal do controlador e sinais de load e reset
+			regA, regB, writeR				:IN	STD_LOGIC_VECTOR(2 DOWNTO 0); 		-- Controle dos registradores a serem usados
+			data							:IN	STD_LOGIC_VECTOR(7 DOWNTO 0); 		-- Informações a serem escritas em um registrador
+			R0, R1, R2, R3, R4, R5, R6, R7	:BUFFER STD_LOGIC_VECTOR(7 DOWNTO 0); 	-- Registradores
+			Rdata1, Rdata2					:OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); 	-- saída de informações dos registradores
 END REGS;
 	
 	ARCHITECTURE options OF REGS IS
-	--SIGNAL R0, R1, R2, R3				: STD_LOGIC_VECTOR(7 DOWNTO 0);
 	BEGIN
-	PROCESS (RegWrite, regA, regB, writeR, data, load, reset, R0, R1, R2, R3)
+	PROCESS (RegWrite, regA, regB, writeR, data, load, reset, R0, R1, R2, R3, R4, R5, R6, R7)
 	BEGIN
 	
 		CASE RESET IS -- RESET zera todos os registradores
@@ -23,22 +21,26 @@ END REGS;
 				R1 <= "00000000";
 				R2 <= "00000000";
 				R3 <= "00000000";
-				Rdata1 <= "00000000";
-				Rdata2 <= "00000000";
+				R4 <= "00000000";
+				R5 <= "00000000";
+				R6 <= "00000000";
+				R7 <= "00000000";
 				
 			WHEN OTHERS => NULL;
 			
 		END CASE;
 		
-		CASE LOAD IS	-- Load carrega os valores de 0 a 3 em cada registrador
+		CASE LOAD IS	-- Load carrega os valores de 0 a 7 em cada registrador
 		
 			WHEN '1' =>
 				R0 <= "00000000";
 				R1 <= "00000001";
 				R2 <= "00000010";
 				R3 <= "00000011";
-				Rdata1 <= "00000000";
-				Rdata2 <= "00000000";
+				R4 <= "00000100";
+				R5 <= "00000101";
+				R6 <= "00000110";
+				R7 <= "00000111";
 				
 			WHEN OTHERS => NULL;
 			
@@ -49,35 +51,59 @@ END REGS;
 			WHEN '0' => -- Sem escrita
 				CASE regA IS
 				
-					WHEN "00" =>
+					WHEN "000" =>
 						Rdata1 <= R0; -- Lê de R0 para a saída A
 						
-					WHEN "01" =>
+					WHEN "001" =>
 						Rdata1 <= R1; -- Lê de R1 para a saída A
 						
-					WHEN "10" =>
+					WHEN "010" =>
 						Rdata1 <= R2; -- Lê de R2 para a saída A
 						
-					WHEN "11" =>
+					WHEN "011" =>
 						Rdata1 <= R3; -- Lê de R3 para a saída A
 						
+					WHEN "100" =>
+						Rdata1 <= R4; -- Lê de R4 para a saída A
+						
+					WHEN "101" =>
+						Rdata1 <= R5; -- Lê de R5 para a saída A
+						
+					WHEN "110" =>
+						Rdata1 <= R6; -- Lê de R6 para a saída A
+						
+					WHEN "111" =>
+						Rdata1 <= R7; -- Lê de R7 para a saída A
+
 					WHEN OTHERS => NULL;
 					
 				END CASE;
 				
 				CASE regB IS
 				
-					WHEN "00" =>
+					WHEN "000" =>
 						Rdata2 <= R0; -- Lê de R0 para a saída B
 						
-					WHEN "01" =>
+					WHEN "001" =>
 						Rdata2 <= R1; -- Lê de R1 para a saída B
 						
-					WHEN "10" =>
+					WHEN "010" =>
 						Rdata2 <= R2; -- Lê de R2 para a saída B
 						
-					WHEN "11" =>
+					WHEN "011" =>
 						Rdata2 <= R3; -- Lê de R3 para a saída B
+
+					WHEN "100" =>
+						Rdata2 <= R4; -- Lê de R4 para a saída B
+						
+					WHEN "101" =>
+						Rdata2 <= R5; -- Lê de R5 para a saída B
+						
+					WHEN "110" =>
+						Rdata2 <= R6; -- Lê de R6 para a saída B
+						
+					WHEN "111" =>
+						Rdata2 <= R7; -- Lê de R7 para a saída B
 						
 					WHEN OTHERS => NULL;
 					
@@ -85,26 +111,46 @@ END REGS;
 			WHEN '1' => -- Com escrita
 				CASE writeR IS
 				
-					WHEN "00" =>
+					WHEN "000" =>
 						R0 <= data; -- Escreve em R0
 						Rdata1 <= "00000000";
 						Rdata2 <= "00000000";
 						
-					WHEN "01" =>
+					WHEN "001" =>
 						R1 <= data; -- Escreve em R1
 						Rdata1 <= "00000000";
 						Rdata2 <= "00000000";
 						
-					WHEN "10" =>
+					WHEN "010" =>
 						R2 <= data; -- Escreve em R2
 						Rdata1 <= "00000000";
 						Rdata2 <= "00000000";
 						
-					WHEN "11" =>
+					WHEN "011" =>
 						R3 <= data; -- Escreve em R3
 						Rdata1 <= "00000000";
 						Rdata2 <= "00000000";
 						
+					WHEN "100" =>
+						R4 <= data; -- Escreve em R4
+						Rdata1 <= "00000000";
+						Rdata2 <= "00000000";
+						
+					WHEN "101" =>
+						R5 <= data; -- Escreve em R5
+						Rdata1 <= "00000000";
+						Rdata2 <= "00000000";
+						
+					WHEN "110" =>
+						R6 <= data; -- Escreve em R6
+						Rdata1 <= "00000000";
+						Rdata2 <= "00000000";
+						
+					WHEN "111" =>
+						R7 <= data; -- Escreve em R7
+						Rdata1 <= "00000000";
+						Rdata2 <= "00000000";
+
 					WHEN OTHERS => NULL;
 					
 				END CASE;
--- ULA.vhd
+++ ULA.vhd
@@ -3,10 +3,10 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_signed.all;
 
 ENTITY ULA IS
-	PORT( Cin			:IN	STD_LOGIC;
+	PORT( 	Cin				:IN	STD_LOGIC;
 			ALUop			:IN	STD_LOGIC_VECTOR(1 DOWNTO 0);
 			X, Y			:IN	STD_LOGIC_VECTOR(7 DOWNTO 0);
-			S					:OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			S				:OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 			Cout,Over,Zero	:OUT STD_LOGIC);
 END ULA;
 	
@@ -19,10 +19,10 @@ END ULA;
 			WHEN "00" => -- AND
 				S <= X AND Y;
 				
-				Cout <= '0';															-- Atribui 0 para overflow e carry out
+				Cout <= '0';													-- Atribui 0 para overflow e carry out
 				Over <= '0';
 				
-				IF ((X AND Y) = 0) THEN												-- Verifica se o resultado da operação é 0
+				IF ((X AND Y) = 0) THEN											-- Verifica se o resultado da operação é 0
 					Zero <= '1';
 				ELSE 
 					Zero <= '0';
@@ -31,7 +31,7 @@ END ULA;
 			WHEN "01" => -- OR
 				S <= X OR Y; 
 				
-				Cout <= '0';															-- Atribui 0 para overflow e carry out
+				Cout <= '0';													-- Atribui 0 para overflow e carry out
 				Over <= '0';
 				
 				IF (X = 0) AND (Y = 0) THEN										-- Verifica se o resultado da operação é 0
@@ -43,12 +43,12 @@ END ULA;
 			WHEN "10" => -- ADD
 				Result <= ('0' & X) + Y + Cin;									-- Realiza a soma e guarda em uma variável auxiliar de 1 bit adicional
 				
-				S <= Result(7 DOWNTO 0);											-- Guarda a soma no output
+				S <= Result(7 DOWNTO 0);										-- Guarda a soma no output
 				
-				Cout <= Result(8);													-- Verifica se houve Cout
-				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);			-- Verifica se houve Overflow
+				Cout <= Result(8);												-- Verifica se houve Cout
+				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);				-- Verifica se houve Overflow
 				
-				IF ((X + Y) = 0) THEN												-- Verifica se o resultado da operação é 0
+				IF ((X + Y) = 0) THEN											-- Verifica se o resultado da operação é 0
 					Zero <= '1';
 				ELSE 
 					Zero <= '0';
@@ -57,10 +57,10 @@ END ULA;
 			WHEN "11" => -- SUB
 				S <= X - Y;
 				
-				Cout <= Result(8);													-- Verifica se houve Cout
-				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);			-- Verifica se houve Overflow
+				Cout <= Result(8);												-- Verifica se houve Cout
+				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);				-- Verifica se houve Overflow
 				
-				IF ((X - Y) = 0) THEN												-- Verifica se o resultado da operação é 0
+				IF ((X - Y) = 0) THEN											-- Verifica se o resultado da operação é 0
 					Zero <= '1';
 				ELSE 
 					Zero <= '0';
