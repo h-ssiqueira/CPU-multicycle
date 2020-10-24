4ee4c69dda9b0b6a09356139b69ffbee2a457aac
Henrique <hss@outlook.com.br>
2020-10-23 22:29:56 -0300

    Remoção do sinal Zero da ULA

--- ULA.vhd
+++ ULA.vhd
@@ -7,7 +7,7 @@ ENTITY ULA IS
 			ALUop			:IN	STD_LOGIC_VECTOR(1 DOWNTO 0);
 			X, Y			:IN	STD_LOGIC_VECTOR(7 DOWNTO 0);
 			S				:OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-			Cout,Over,Zero	:OUT STD_LOGIC);
+			Cout,Over		:OUT STD_LOGIC);
 END ULA;
 	
 	ARCHITECTURE options OF ULA IS
@@ -22,23 +22,11 @@ END ULA;
 				Cout <= '0';													-- Atribui 0 para overflow e carry out
 				Over <= '0';
 				
-				IF ((X AND Y) = 0) THEN											-- Verifica se o resultado da operação é 0
-					Zero <= '1';
-				ELSE 
-					Zero <= '0';
-				END IF;
-				
 			WHEN "01" => -- OR
 				S <= X OR Y; 
 				
 				Cout <= '0';													-- Atribui 0 para overflow e carry out
 				Over <= '0';
-				
-				IF (X = 0) AND (Y = 0) THEN										-- Verifica se o resultado da operação é 0
-					Zero <= '1';
-				ELSE 
-					Zero <= '0';
-				END IF;
 				
 			WHEN "10" => -- ADD
 				Result <= ('0' & X) + Y + Cin;									-- Realiza a soma e guarda em uma variável auxiliar de 1 bit adicional
@@ -48,23 +36,11 @@ END ULA;
 				Cout <= Result(8);												-- Verifica se houve Cout
 				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);				-- Verifica se houve Overflow
 				
-				IF ((X + Y) = 0) THEN											-- Verifica se o resultado da operação é 0
-					Zero <= '1';
-				ELSE 
-					Zero <= '0';
-				END IF;
-				
 			WHEN "11" => -- SUB
 				S <= X - Y;
 				
 				Cout <= Result(8);												-- Verifica se houve Cout
 				Over <= Result(8) XOR X(7) XOR Y(7) XOR Result(7);				-- Verifica se houve Overflow
-				
-				IF ((X - Y) = 0) THEN											-- Verifica se o resultado da operação é 0
-					Zero <= '1';
-				ELSE 
-					Zero <= '0';
-				END IF;
 				
 			WHEN OTHERS => NULL;
 		END CASE; 
