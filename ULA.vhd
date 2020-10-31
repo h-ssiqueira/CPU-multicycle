7ec52a0ac08e4bdbd63c570f03aeb66f976166f7
Henrique <hss@outlook.com.br>
2020-10-31 13:55:28 -0300

    ULA funcionando

--- ULA.vhd
+++ ULA.vhd
@@ -10,10 +10,10 @@ ENTITY ULA IS
 END ULA;
 
 	ARCHITECTURE options OF ULA IS
-	SIGNAL Result : STD_LOGIC_VECTOR(8 DOWNTO 0);
+	--SIGNAL Result : STD_LOGIC_VECTOR(8 DOWNTO 0);
 	SIGNAL RB, RA : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	BEGIN
-	PROCESS (ALUop, A, B, Result, ALUSrcA,ALUSrcB,clock, PC,Imed)
+	PROCESS (ALUop, A, B, ALUSrcA,ALUSrcB,clock, PC,Imed)
 	BEGIN
 		IF clock'EVENT AND clock = '1' THEN
 			CASE ALUSrcB  IS
@@ -33,6 +33,7 @@ END ULA;
 					RA <= A;
 				WHEN OTHERS => NULL;
 				END CASE;
+		ELSIF clock'EVENT AND clock = '0' THEN
 			CASE ALUop IS
 				WHEN "00" => -- AND
 
@@ -42,14 +43,17 @@ END ULA;
 					ALUout <= RA OR RB; 
 					
 				WHEN "10" => -- ADD
-					Result <= ('0'& RA) + RB ;	 	--- Realiza a soma e guarda em uma variável auxiliar de 1 bit adicional
+					--Result <= ('0'& RA) + RB ;	 	--- Realiza a soma e guarda em uma variável auxiliar de 1 bit adicional
 					
-					ALUout <= Result(7 DOWNTO 0);	-- Guarda a soma no output
+					--ALUout <= Result(7 DOWNTO 0);	-- Guarda a soma no output
+					ALUout <= RA + RB;
 					
 				WHEN "11" => -- SUB
-					Result <= ('0'& RA) - RB;		--Realiza a subtracao e guarda em uma variavel auxiliar de 1 bit adicional
+					--Result <= ('0'& RA) - RB;		--Realiza a subtracao e guarda em uma variavel auxiliar de 1 bit adicional
 					
-					ALUout <= Result(7 DOWNTO 0);	-- Guarda a soma no output
+					--ALUout <= Result(7 DOWNTO 0);	-- Guarda a soma no output
+					ALUout <= RA - RB;
+					
 				WHEN OTHERS => NULL;
 			END CASE; 
 		END IF;
